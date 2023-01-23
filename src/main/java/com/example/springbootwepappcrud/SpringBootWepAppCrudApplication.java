797c3021aac31a7b1fe1147993bbcc3f346d90c5
Yavuz Özbay <ozbayavz@gmail.com>
2023-01-23 18:10:24 +0100

    Initial commit

---
+++ src/main/java/com/example/springbootwepappcrud/SpringBootWepAppCrudApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootwepappcrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootWepAppCrudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootWepAppCrudApplication.class, args);
+    }
+
+}
